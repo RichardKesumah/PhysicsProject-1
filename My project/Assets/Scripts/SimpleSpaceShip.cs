a9867da6ebe034996d92de081a11a37715cba5fd
RichardKesumah <90250875+RichardKesumah@users.noreply.github.com>
2023-12-05 23:55:32 +0800

    killme

---
+++ My project/Assets/Scripts/SimpleSpaceShip.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpaceShip : MonoBehaviour
+{
+    float speed = 5f;
+
+    void Update()
+    {
+    transform.Translate(new Vector3(
+        Input.GetAxis("Horizontal") * speed,
+        Input.GetAxis("Vertical") * speed,
+        0) * Time.deltaTime);
+    }
+}
